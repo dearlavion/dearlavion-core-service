4718c4a998c7abec5a9d7aac1a9094353e428cd4
alysson <alysson@Alyssons-MacBook-Air.local>
2025-12-10 08:32:03 +0800

    allow ngrok url in cors

--- src/main/java/com/dearlavion/coreservice/config/SecurityConfig.java
+++ src/main/java/com/dearlavion/coreservice/config/SecurityConfig.java
@@ -52,7 +52,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200")); // your Angular app
+        configuration.setAllowedOriginPatterns(List.of(
+                "http://localhost:4200",
+                "https://*.ngrok.pizza"
+        )); // your Angular app
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // needed if you send cookies or Authorization headers
