bfda618772966fdb95ed7c85d88ed635c8b778d2
alysson <alysson@Alyssons-MacBook-Air.local>
2026-01-20 21:11:04 +0800

    redis fix

--- src/main/java/com/dearlavion/coreservice/config/RedisConfig.java
+++ src/main/java/com/dearlavion/coreservice/config/RedisConfig.java
@@ -1,9 +1,12 @@
 package com.dearlavion.coreservice.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -12,6 +15,20 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @RequiredArgsConstructor
 @Configuration
 public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(
+            @Value("${spring.redis.host}") String host,
+            @Value("${spring.redis.port}") int port) {
+        System.out.println("[Redis] host: "+host);
+        System.out.println("[Redis] port: "+port);
+
+        RedisStandaloneConfiguration config =
+                new RedisStandaloneConfiguration(host, port);
+
+        return new LettuceConnectionFactory(config);
+    }
+
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(
