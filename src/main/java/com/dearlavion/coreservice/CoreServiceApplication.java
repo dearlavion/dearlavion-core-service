e22aa52a28c2e069116ec45a3a3903d270efeddb
alysson <alysson@Alyssons-MacBook-Air.local>
2026-02-01 20:57:00 +0800

    fix  redis cache

--- src/main/java/com/dearlavion/coreservice/CoreServiceApplication.java
+++ src/main/java/com/dearlavion/coreservice/CoreServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
 @EnableCaching
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration.class})
 public class CoreServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CoreServiceApplication.class, args);
